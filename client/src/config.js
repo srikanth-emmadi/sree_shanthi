53f06ed7884e8c93c0eb2ce223f45bf15630f27f
srikanth <esrikanth.011@gmail.com>
2026-03-17 00:08:47 +0530

    Update config file

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,2 @@
 
-// client/src/config.js
-const API_URL = 
-  process.env.REACT_APP_API_URL || "http://localhost:5000";
-
-export default API_URL;
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
